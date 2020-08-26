0ffaebba38aedd10f96924e35cd906adb3ba5972
hexiaoxing <33343368@qq.com>
2020-08-26 13:26:57 +0800

    增加跨域过滤器

---
+++ src/main/java/com/yuqiaotech/common/filter/CrossFilter.java
@@ -0,0 +1,36 @@
+package com.yuqiaotech.common.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@WebFilter("/*")
+public class CrossFilter implements Filter
+{
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+        throws IOException, ServletException
+    {
+        HttpServletRequest request = (HttpServletRequest)req;
+        HttpServletResponse response = (HttpServletResponse)res;
+        
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Headers",
+            "Content-Type,Content-Length, Authorization, Accept,X-Requested-With,X-App-Id, X-Token,Origin");
+        response.setHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS,HEAD");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        
+        chain.doFilter(request, response);
+    }
+}

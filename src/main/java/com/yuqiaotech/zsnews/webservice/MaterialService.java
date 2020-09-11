d1af10c19dc887c6f9a4eea9ae8e8ada9d7e080c
yinchaodear <739822061@qq.com>
2020-09-11 14:18:34 +0800

    添加首页的推荐配置

---
+++ src/main/java/com/yuqiaotech/zsnews/webservice/MaterialService.java
@@ -0,0 +1,64 @@
+package com.yuqiaotech.zsnews.webservice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yuqiaotech.common.web.base.BaseController;
+import com.yuqiaotech.common.web.base.BaseRepository;
+import com.yuqiaotech.common.web.domain.response.Result;
+import com.yuqiaotech.zsnews.model.HistorySearchRecord;
+import com.yuqiaotech.zsnews.model.Material;
+
+@RestController
+@RequestMapping("zsnews/material")
+public class MaterialService extends BaseController
+{
+	@Autowired
+    private BaseRepository<Material, Long> materialRepository;
+	
+	/*
+	 * 只针对首页的
+	 */	
+    @GetMapping("materialswiper")
+    public Result MaterialSwiper(@RequestParam Long columid,@RequestParam Long channelid)
+    {
+        System.out.println("MaterialService.MaterialSwiper()"+columid+channelid);
+        String sql ="SELECT  t.f_id  ,f_picpath FROM t_pic_mapping  m inner join t_material t on  m.f_material_id = t.f_id  "
+        		+ "inner join t_channel c on t.f_channel_id = c.f_id where t.f_channel_id = "+channelid+" and t.f_column_id  = "+columid;
+        List pic = materialRepository.findMapByNativeSql(sql);
+        Map result = new HashMap<>();
+        result.put("pic",pic);
+        return success(result);
+    }
+    
+    
+	/*
+	 * 只针对首页的
+	 */	
+    @GetMapping("materialswiperrecommod")
+    public Result MaterialSwiperRecommod(@RequestParam Long columid,@RequestParam String type)
+    {
+        System.out.println("MaterialService.MaterialSwiper()"+columid+type);
+        Long channelid =null;
+        if("推荐".equals(type)){    
+        	Material r = materialRepository.findUniqueBy("title", "推荐", Material.class);
+        	if(r!=null){
+        		channelid =r.getChannel().getId();
+        	}
+        }
+        String sql ="SELECT  t.f_id  ,f_picpath FROM t_pic_mapping  m inner join t_material t on  m.f_material_id = t.f_id  "
+        		+ "inner join t_channel c on t.f_channel_id = c.f_id where t.f_channel_id = "+channelid+" and t.f_column_id  = "+columid;
+        List pic = materialRepository.findMapByNativeSql(sql);
+        Map result = new HashMap<>();
+        result.put("pic",pic);
+        return success(result);
+    }
+	
+}

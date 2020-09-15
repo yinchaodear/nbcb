d5ca45ecf56c1a6b6ef29b3a6f17144e8bed8196
yinchaodear <739822061@qq.com>
2020-09-15 18:10:12 +0800

    添加用户注册的页面

--- src/main/java/com/yuqiaotech/zsnews/webservice/ChannelService.java
+++ src/main/java/com/yuqiaotech/zsnews/webservice/ChannelService.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.yuqiaotech.common.tools.UcpaasSms.UcpaasSms;
 import com.yuqiaotech.common.web.base.BaseController;
 import com.yuqiaotech.common.web.base.BaseRepository;
 import com.yuqiaotech.common.web.domain.response.Result;
@@ -23,6 +24,7 @@ import com.yuqiaotech.zsnews.model.Category;
 import com.yuqiaotech.zsnews.model.Channel;
 import com.yuqiaotech.zsnews.model.ChannelFollower;
 import com.yuqiaotech.zsnews.model.Column;
+import com.yuqiaotech.zsnews.model.UserInfo;
 
 /**
  * 频道管理
@@ -47,6 +49,8 @@ public class ChannelService extends BaseController
     @Autowired
     private BaseRepository<Column, Long> columnRepository;
     
+    @Autowired
+    private BaseRepository<UserInfo, Long> userInfoRepository;
     /*
      * 对于出现在首页的频道的代码,只取频道
      */
@@ -176,12 +180,74 @@ public class ChannelService extends BaseController
      * 用户注册
      */
     @RequestMapping("register")
-    public Result Register(@RequestBody User user) {
+    public Result Register(@RequestBody UserInfo user) {
        System.out.println("ChannelService.Register()");
+       Map result = new HashMap<>();   
        System.err.println(user.getUsername());
        System.err.println(user.getMobile());
-       System.err.println(user.getPassword());     	
-       Map result = new HashMap<>();    	
+       System.err.println(user.getPwd());
+       String sql ="SELECT * FROM t_user_info where f_username ='"+user.getUsername()+"'";
+       List map =  userInfoRepository.findMapByNativeSql(sql);
+       if(!map.isEmpty()){
+    	   result.put("msg", 1); //1 用户名重复
+    	   return success(result);
+       }
+       sql ="SELECT * FROM t_user_info where f_mobile ="+user.getMobile();
+       map =  userInfoRepository.findMapByNativeSql(sql);
+       if(!map.isEmpty()){
+    	   result.put("msg", 2); //2 电话重复
+    	   return success(result);
+       }
+       
+       userInfoRepository.save(user);
+       result.put("msg", 0); //0 成功
        return success(result);
     }
+    
+    /*
+     * 用户注册
+     */
+    @RequestMapping("registercode")
+    public Result RegisterCode(@RequestBody User user) {
+       System.out.println("ChannelService.RegisterCode()");
+       Map result = new HashMap<>();
+		try {
+			/**
+			 * 这里还要加上发送短信验证码的代码
+			 */
+			String mobile = user.getMobile();
+//			String sql = " select f_item_value from t_sys_config where f_item_name='ucpassTemplateId'";
+//			Map ucpassTemplateMap = manager.queryUniqueMapByNativeSql(sql, null);
+//			String templateId = "";
+//			if (ucpassTemplateMap != null) {
+//				templateId = (String) ucpassTemplateMap.get("f_item_value");
+//			}
+			String templateId ="474082";//这里先用写死的
+			String number = "";
+			try {
+				number = GenerateNumber();
+				System.err.println(number);
+				String coderesult = UcpaasSms.templateSMS(templateId, mobile, number);
+				result.put("error", coderesult);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				result.put("error", e.getMessage());
+			}
+			result.put("msg", "success");
+			result.put("code", number);			
+			return success(result);
+		} catch (Exception e) {
+			result.put("msg", "error");
+			result.put("reason", e.getMessage());
+			return success(result);
+		}
+    }
+    
+   
+
+	private String GenerateNumber() {
+		int x = (int) (1 + Math.random() * (9999 - 1 + 1));
+		String param = String.format("%04d", x);// 前面补0
+		return param;
+	}
 }

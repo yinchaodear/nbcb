e86ddae1cb719ca2964bc0f5bb81ebeb664f494a
yinchaodear <739822061@qq.com>
2020-09-16 12:46:44 +0800

    单独分出来注册，修改政务部分的代码

--- src/main/java/com/yuqiaotech/zsnews/webservice/ChannelService.java
+++ src/main/java/com/yuqiaotech/zsnews/webservice/ChannelService.java
@@ -175,79 +175,20 @@ public class ChannelService extends BaseController
     }
     
     
-    
     /*
-     * 用户注册
+     * 获取政务的title
      */
-    @RequestMapping("register")
-    public Result Register(@RequestBody UserInfo user) {
-       System.out.println("ChannelService.Register()");
-       Map result = new HashMap<>();   
-       System.err.println(user.getUsername());
-       System.err.println(user.getMobile());
-       System.err.println(user.getPwd());
-       String sql ="SELECT * FROM t_user_info where f_username ='"+user.getUsername()+"'";
-       List map =  userInfoRepository.findMapByNativeSql(sql);
-       if(!map.isEmpty()){
-    	   result.put("msg", 1); //1 用户名重复
-    	   return success(result);
-       }
-       sql ="SELECT * FROM t_user_info where f_mobile ="+user.getMobile();
-       map =  userInfoRepository.findMapByNativeSql(sql);
-       if(!map.isEmpty()){
-    	   result.put("msg", 2); //2 电话重复
-    	   return success(result);
-       }
-       
-       userInfoRepository.save(user);
-       result.put("msg", 0); //0 成功
-       return success(result);
+    @GetMapping("governmenttitle")
+    public Result GovernMentTitle()
+    {
+        System.out.println("ChannelService.GovernMentTitle()");
+        String sql = "SELECT t.f_title,t.f_id as f_id FROM t_channel t  "
+            + "  where t.f_kind='政务'  and t.f_status= 0 ";
+        List mymenu = channelRepository.findMapByNativeSql(sql);
+        Map result = new HashMap<>();
+        result.put("mymenu", mymenu);
+        return success(result);
     }
     
-    /*
-     * 用户注册
-     */
-    @RequestMapping("registercode")
-    public Result RegisterCode(@RequestBody User user) {
-       System.out.println("ChannelService.RegisterCode()");
-       Map result = new HashMap<>();
-		try {
-			/**
-			 * 这里还要加上发送短信验证码的代码
-			 */
-			String mobile = user.getMobile();
-//			String sql = " select f_item_value from t_sys_config where f_item_name='ucpassTemplateId'";
-//			Map ucpassTemplateMap = manager.queryUniqueMapByNativeSql(sql, null);
-//			String templateId = "";
-//			if (ucpassTemplateMap != null) {
-//				templateId = (String) ucpassTemplateMap.get("f_item_value");
-//			}
-			String templateId ="474082";//这里先用写死的
-			String number = "";
-			try {
-				number = GenerateNumber();
-				System.err.println(number);
-				String coderesult = UcpaasSms.templateSMS(templateId, mobile, number);
-				result.put("error", coderesult);
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-				result.put("error", e.getMessage());
-			}
-			result.put("msg", "success");
-			result.put("code", number);			
-			return success(result);
-		} catch (Exception e) {
-			result.put("msg", "error");
-			result.put("reason", e.getMessage());
-			return success(result);
-		}
-    }
-    
-   
-
-	private String GenerateNumber() {
-		int x = (int) (1 + Math.random() * (9999 - 1 + 1));
-		String param = String.format("%04d", x);// 前面补0
-		return param;
-	}
+  
 }
